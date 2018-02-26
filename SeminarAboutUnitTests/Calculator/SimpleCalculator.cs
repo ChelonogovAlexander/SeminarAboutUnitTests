602735d45d879f27404204c318fd9f3eccce91da
chelonogovae <chelonogovae@it2g.ru>
2018-02-26 22:03:47 +0300

    SimpleCalculator with tests

---
+++ SeminarAboutUnitTests/Calculator/SimpleCalculator.cs
@@ -0,0 +1,15 @@
+﻿namespace SeminarAboutUnitTests.Calculator
+{
+    /// <summary> Простой калькулятор </summary>
+    public class SimpleCalculator
+    {
+        /// <summary> Суммируем числа <see cref="a"/> и <see cref="b"/> </summary>
+        /// <param name="a">Первое слагаемое</param>
+        /// <param name="b">Второе слагаемое</param>
+        /// <returns>Сумма двух слагаемых</returns>
+        public int Sum(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Tests/Calculator/SimpleCalculatorTests.cs
@@ -0,0 +1,99 @@
+﻿using NUnit.Framework;
+using SeminarAboutUnitTests.Calculator;
+
+namespace Tests.Calculator
+{
+    /// <summary> Тесты на <see cref="SimpleCalculator"/> </summary>
+    [TestFixture]
+    public class SimpleCalculatorTests
+    {
+        private SimpleCalculator _service;
+
+        /// <summary> Перед каждым тестом </summary>
+        [SetUp]
+        public void Setup()
+        {
+            _service = GetService();
+        }
+
+        /// <summary> Проверяем, что возвращается 0, если складываем отрицательное число и модуль отрицательного числа </summary>
+        [Test]
+        public void Test_Sum_Return_0_If_Sum_Negative_Number_With_ModuleOfNegative()
+        {
+            //Arrange
+            var firstArgument = -123;
+            var secondArgument = 123;
+
+            //Act
+            var act = _service.Sum(firstArgument, secondArgument);
+
+            //Assert
+            Assert.AreEqual(0, act);
+        }
+
+        /// <summary> Проверяем, что возвращается 4, если складываем 2+2 </summary>
+        [Test]
+        public void Test_Sum_Return_4_If_Sum_2_And_2()
+        {
+            //Arrange
+            var firstArgument = 2;
+            var secondArgument = 2;
+
+            //Act
+            var act = _service.Sum(firstArgument, secondArgument);
+
+            //Assert
+            Assert.AreEqual(4, act);
+        }
+
+        /// <summary> Проверяем, что возвращается 0, если оба аргумента = 0 </summary>
+        [Test]
+        public void Test_Sum_Return_0_If_Both_Arguments_Are_0()
+        {
+            //Arrange
+            var firstArgument = 0;
+            var secondArgument = 0;
+
+            //Act
+            var act = _service.Sum(firstArgument, secondArgument);
+
+            //Assert
+            Assert.AreEqual(0, act);
+        }
+
+        /// <summary> Проверяем, что возвращается первый аргумент, если второй равен 0 </summary>
+        [Test]
+        public void Test_Sum_Return_First_Argument_If_Second_Argument_Is_0()
+        {
+            //Arrange
+            var firstArgument = 1234;
+            var secondArgument = 0;
+
+            //Act
+            var act = _service.Sum(firstArgument, secondArgument);
+
+            //Assert
+            Assert.AreEqual(firstArgument, act);
+        }
+
+        /// <summary> Проверяем, что возвращается второй аргумент, если первый равен 0 </summary>
+        [Test]
+        public void Test_Sum_Return_Second_Argument_If_First_Argument_Is_0()
+        {
+            //Arrange
+            var firstArgument = 0;
+            var secondArgument = 1234;
+
+            //Act
+            var act = _service.Sum(firstArgument, secondArgument);
+
+            //Assert
+            Assert.AreEqual(secondArgument, act);
+        }
+
+        private SimpleCalculator GetService()
+        {
+            return new SimpleCalculator();
+        }
+    }
+}
